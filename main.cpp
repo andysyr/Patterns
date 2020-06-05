ece279c2049c6b7c0de656c49efc2802e5cbb162
andysyr <andrey.1999.11.12@gmail.com>
2020-06-05 20:04:42 +0300

    add structural patterns

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include "creational_patterns.h"
+#include "structural_patterns.h"
 
 using namespace std;
 
 int main()
 {
+    std::cout<<"Lab 10: Creational patterns"<<std::endl;
     Creational_patterns();
+    std::cout<<std::endl;
+
+    std::cout<<"Lab 10: Creational patterns"<<std::endl;
+    Structural_patterns();
+    std::cout<<std::endl;
     return 0;
 }
